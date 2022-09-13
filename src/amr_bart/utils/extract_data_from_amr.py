bd09f147142aa03d9702adc067b5b78938185829
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-09-13 16:19:02 +0200

    Write all output to the dout directory
    Note that this requires that the path structure is retained and that non-English amrs are in a directory "amrs_XX"

--- src/amr_bart/utils/extract_data_from_amr.py
+++ src/amr_bart/utils/extract_data_from_amr.py
@@ -23,21 +23,27 @@ def extract_text_from_amr(pfin: Path):
 
 
 def main(*indirs: Union[str, PathLike], dout: Union[str, PathLike]):
+    pdout = Path(dout).resolve()
+    pdout.mkdir(exist_ok=True, parents=True)
+
     ft_datasets = {"train": [], "dev": [], "test": []}
     for indir in tqdm(indirs, unit="language", position=0):
         pdir = Path(indir).resolve()
+        amrs_dir_name = pdir.parent.name
+        lang = amrs_dir_name.split("_")[-1] if "_" in amrs_dir_name else "en"
+        lang_dir = pdout.joinpath(lang)
+        lang_dir.mkdir(exist_ok=True, parents=True)
+
         split_dirs = [p for p in pdir.glob("*") if p.is_dir() and p.name in {"dev", "test", "training"}]
         for split_dir in tqdm(split_dirs, unit="split", leave=False, position=1):
             split_name = get_split_name(split_dir)
-
-            with split_dir.joinpath(f"{split_name}.txt").open("w", encoding="utf-8") as fhout:
+            with lang_dir.joinpath(f"{lang}_{split_name}.txt").open("w", encoding="utf-8") as fhout:
                 for pfin in tqdm(list(split_dir.glob("*.txt")), unit="file", leave=False, position=2):
                     sentences = extract_text_from_amr(pfin)
                     fhout.write("\n".join(sentences) + "\n")
                     ft_datasets[split_name].extend(sentences)
 
-    pdout = Path(dout).resolve()
-    pdout.mkdir(exist_ok=True, parents=True)
+
     for split_name, sentences in ft_datasets.items():
         text = "\n".join(sentences) + "\n"
         pdout.joinpath(f"merged-{split_name}.txt").write_text(text, encoding="utf-8")
