2b2e2cfecfdc32a5dabc9cbc2448df3ef4c053c7
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-09-22 11:39:34 +0200

    add trainer-related functions

---
+++ src/amr_bart/amr_bart/trainer_amr_bart.py
@@ -0,0 +1,30 @@
+import evaluate
+from transformers import Trainer
+
+
+class AMRTrainer(Trainer):
+    pass
+
+
+# Some trainer-specific submethods that may be relevant:
+def preprocess_logits_for_metrics(logits, labels):
+    if isinstance(logits, tuple):
+        # Depending on the model and config, logits may contain extra tensors,
+        # like past_key_values, but logits always come first
+        logits = logits[0]
+    return logits.argmax(dim=-1)
+
+
+metric = evaluate.load("accuracy")
+
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    # preds have the same shape as the labels, after the argmax(-1) has been calculated
+    # by preprocess_logits_for_metrics
+    labels = labels.reshape(-1)
+    preds = preds.reshape(-1)
+    mask = labels != -100
+    labels = labels[mask]
+    preds = preds[mask]
+    return metric.compute(predictions=preds, references=labels)
