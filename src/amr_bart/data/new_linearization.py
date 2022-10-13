6cfdabe25c7cb0ad7fd1dc114c7276ffb719824e
BramVanroy <Bram.Vanroy@UGent.be>
2022-10-13 17:16:23 +0200

    add iteration

--- src/amr_bart/data/new_linearization.py
+++ src/amr_bart/data/new_linearization.py
@@ -3,12 +3,17 @@ from typing import List, Iterable, Union
 
 from .penman import load
 
+import penman
+from penman import Tree
+from penman.tree import is_atomic
+
 """
 TODO: build DFS linearizer from scratch based on the paper description page 12566 and figure 1
 https://ojs.aaai.org/index.php/AAAI/article/view/17489
-Of course use the linearized graph as the labels and (shifted) as the decoder inputs
-update tokenizer with special tokens like :ARG and all others (probably gotta check the guidelines for all possible tags)
-add special tokens for opening/closing brackets
+- Of course use the linearized graph as the labels and (shifted) as the decoder inputs
+- update tokenizer with special tokens like :ARG and all others (probably gotta check the guidelines for all possible tags)
+- add special tokens for opening/closing brackets
+
 https://github.com/amrisi/amr-guidelines/blob/master/amr.md
 Voc; We can try:
     - putting all tokens in the new vocabulary as full tokens (e.g. believe-01); OR
@@ -18,7 +23,6 @@ Custom constrained beam search? (if a token is not allowed in a position, set it
     - a sense dash must be followed by a sense
     - special reference token (<R0>) can only follow 1. an opening bracket or 2. an arg:
 """
-
 def read_graphs(
     paths: Union[List[Union[str, PathLike]], Union[str, PathLike]],
     use_recategorization=False,
@@ -43,3 +47,56 @@ def read_graphs(
             )
 
     return graphs
+
+
+
+
+def format_sense(idx: int):
+    if 0 < idx < 25:
+        return f"<sense-id:{idx}>"
+    else:
+        return "<sense-id:x>"
+
+
+def format_relation(relation_type: str):
+    if relation_type == "/":
+        return "<relation_type:instance>"
+    else:
+        return f"<sense-id:{relation_type.replace(':', '')}>"
+
+
+def serialize_node(parent_node, descriptor=None, varname=None, is_root=True):
+    if is_root:
+        print("OPEN TREE")
+    if is_atomic(parent_node):
+        print("TERMINAL", parent_node, descriptor, varname)
+    else:
+        if not isinstance(parent_node, Tree):
+            parent_node = Tree(parent_node)
+
+        varname, branches = parent_node.node
+
+        if descriptor is not None and varname is not None:
+            print("OPEN", descriptor, varname)
+
+        for descriptor, target in branches:
+            serialize_node(target, descriptor, varname, is_root=False)
+
+        if descriptor is not None and varname is not None:
+            print("CLOSE", descriptor, varname)
+
+    if is_root:
+        print("CLOSE TREE")
+
+
+if __name__ == '__main__':
+    penman_str = """
+    (s / sleep
+        :ARG0 (d / dog
+            :ARG0-of (b / bark-01)
+        )
+    )
+    """
+    tree = penman.parse(penman_str)
+
+    serialize_node(tree)
