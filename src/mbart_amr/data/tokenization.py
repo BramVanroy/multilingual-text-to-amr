9d11167c6c9f7d5485290df24a167769aefb5f62
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-11-08 14:01:40 +0100

    explicitly use MBartTokenizer instead of super

--- src/mbart_amr/data/tokenization.py
+++ src/mbart_amr/data/tokenization.py
@@ -55,8 +55,8 @@ def clean_up_tokenization(out_string: str) -> str:
 
 class AMRMBartTokenizer(MBartTokenizer):
     @classmethod
-    def from_pretrained(cls, *args, new_tokens_file: Optional[str] = None, **kwargs) -> AMRMBartTokenizer:
-        inst = super(AMRMBartTokenizer, cls).from_pretrained(*args, **kwargs)
+    def from_pretrained(cls, *args, new_tokens_file: Optional[str] = None, **kwargs):
+        inst = MBartTokenizer.from_pretrained(*args, **kwargs)
 
         new_tokens_file = (
             new_tokens_file
