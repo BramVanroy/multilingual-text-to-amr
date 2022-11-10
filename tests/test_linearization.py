23f437e4c449239974ef0b646bc9d647447c14b8
Bram Vanroy <Bram.Vanroy@UGent.be>
2022-11-10 16:56:58 +0100

    update tests
    Now also checks 1. with/without wiki 2. whether the string parses with smatch's own amr.AMR parser (and not just with penman)

--- tests/test_linearization.py
+++ tests/test_linearization.py
@@ -1,25 +1,53 @@
 from pathlib import Path
 from tqdm import tqdm
 import penman
+from ftfy import fix_text
 
-from mbart_amr.data.linearization import penmantree2linearized, linearized2penmantree
+from amr import AMR
+
+from mbart_amr.data.linearization import penmantree2linearized, linearized2penmanstr, do_remove_wiki
 
 
 def main(indir: str):
     pdin = Path(indir)
-    for pfin in tqdm(list(pdin.rglob("*.txt")), unit="file"):
-        with pfin.open(encoding="utf-8") as fhin:
-            for tree in penman.iterparse(fhin):
-                tree.reset_variables()
 
-                linearized = penmantree2linearized(tree)
-                delinearized_tree = linearized2penmantree(linearized)
+    for remove_wiki in (True, False):
+        for pfin in tqdm(list(pdin.rglob("*.txt")), unit="file", desc=f"Remove wiki? {remove_wiki}"):
+            with pfin.open(encoding="utf-8") as fhin:
+                for tree in penman.iterparse(fhin):
+                    tree.reset_variables()
+                    # NOTE: the fix_text is important to make sure the reference tree also is correctly formed, e.g.
+                    # (':op2', '"d’Intervention"'), -> (':op2', '"d\'Intervention"'),
+                    penman_str = fix_text(penman.format(tree))
 
-                if tree != delinearized_tree:
-                    print(tree.metadata)
-                    print(tree)
-                    print(delinearized_tree)
-                    raise ValueError("Tree mismatch between original tree and delinearized tree")
+                    if remove_wiki:
+                        penman_str = do_remove_wiki(penman_str)
+
+                    original_tree = penman.parse(penman_str)
+
+                    linearized = penmantree2linearized(original_tree)
+                    delinearized_penman_str = linearized2penmanstr(linearized)
+                    delinearized_tree = penman.parse(delinearized_penman_str)
+
+                    if original_tree != delinearized_tree:
+                        print(tree.metadata)
+                        print(original_tree)
+                        print(delinearized_tree)
+                        raise ValueError("Tree mismatch between original tree and delinearized tree")
+
+                    try:
+                        # Although we use penman to parse AMR, `smatch` uses its own AMR parser
+                        # so we also verify that our linearization/delinearization works with that
+                        amr_parsed = AMR.parse_AMR_line(delinearized_penman_str)
+
+                        if amr_parsed is None:
+                            raise Exception("Error parsing AMR with amr.AMR library")
+                    except Exception as exc:
+                        print(tree.metadata)
+                        print(original_tree)
+                        print(delinearized_penman_str)
+                        print(delinearized_tree)
+                        raise exc
 
 
 if __name__ == "__main__":
--- tests/test_tokenization.py
+++ tests/test_tokenization.py
@@ -3,42 +3,71 @@ from tqdm import tqdm
 import penman
 from ftfy import fix_text
 
-from mbart_amr.amr_bart.linearization import penmanstr2linearized, linearized2penmantree
-from mbart_amr.amr_bart.tokenization_amr_bart import AMRMBartTokenizer
+from amr import AMR
+
+from mbart_amr.data.linearization import penmanstr2linearized, linearized2penmanstr, do_remove_wiki
+from mbart_amr.data.tokenization import AMRMBartTokenizer
 
 
 def main(indir: str):
     pdin = Path(indir)
     tokenizer = AMRMBartTokenizer.from_pretrained("facebook/mbart-large-cc25", src_lang="en_XX", tgt_lang="amr_XX")
-    for pfin in tqdm(list(pdin.rglob("*.txt")), unit="file"):
-        with pfin.open(encoding="utf-8") as fhin:
-            for tree in penman.iterparse(fhin):
-                tree.reset_variables()
-                # NOTE: the fix_text is important to make sure the reference tree also is correctly formed, e.g.
-                # (':op2', '"d’Intervention"'), -> (':op2', '"d\'Intervention"'),
-                penman_str = fix_text(penman.format(tree))
-                original_tree = penman.parse(penman_str)
 
-                encoded = tokenizer.encode_penmanstrs(penman_str, remove_wiki=False)
-                decoded = tokenizer.decode_and_fix(encoded.input_ids)
+    for remove_wiki in (True, False):
+        for pfin in tqdm(list(pdin.rglob("*.txt")), unit="file", desc=f"Remove wiki? {remove_wiki}"):
+            with pfin.open(encoding="utf-8") as fhin:
+                for tree in penman.iterparse(fhin):
+                    tree.reset_variables()
+                    # NOTE: the fix_text is important to make sure the reference tree also is correctly formed, e.g.
+                    # (':op2', '"d’Intervention"'), -> (':op2', '"d\'Intervention"'),
+                    penman_str = fix_text(penman.format(tree))
 
-                try:
-                    delinearized_tree = linearized2penmantree(decoded)
-                except penman.exceptions.DecodeError as exc:
-                    print(original_tree.metadata)
-                    print(original_tree)
-                    print(decoded)
-                    raise exc
+                    if remove_wiki:
+                        penman_str = do_remove_wiki(penman_str)
 
-                if original_tree != delinearized_tree:
-                    print(tree.metadata)
-                    print("PENMAN", penman_str)
-                    print("LINEARIZED", penmanstr2linearized(penman_str))
-                    print("ENCODED", encoded)
-                    print("DECODED", decoded)
-                    print(original_tree)
-                    print(delinearized_tree)
-                    raise ValueError("Tree mismatch between original tree and delinearized tree")
+                    original_tree = penman.parse(penman_str)
+
+                    encoded = tokenizer.encode_penmanstrs(penman_str, remove_wiki=remove_wiki)
+                    decoded = tokenizer.decode_and_fix(encoded.input_ids)
+
+                    try:
+                        delinearized_penman_str = linearized2penmanstr(decoded)
+                        delinearized_tree = penman.parse(delinearized_penman_str)
+                    except penman.exceptions.DecodeError as exc:
+                        print(tree.metadata)
+                        print(original_tree)
+                        print(decoded)
+                        print(str(pfin))
+                        print(remove_wiki)
+                        raise exc
+
+                    try:
+                        # Although we use penman to parse AMR, `smatch` uses its own AMR parser
+                        # so we also verify that our linearization/delinearization works with that
+                        amr_parsed = AMR.parse_AMR_line(delinearized_penman_str)
+
+                        if amr_parsed is None:
+                            raise Exception("Error parsing AMR with amr.AMR library")
+                    except Exception as exc:
+                        print(tree.metadata)
+                        print(tree)
+                        print(delinearized_penman_str)
+                        print(delinearized_tree)
+                        print(str(pfin))
+                        print(remove_wiki)
+                        raise exc
+
+                    if original_tree != delinearized_tree:
+                        print(tree.metadata)
+                        print("PENMAN", penman_str)
+                        print("LINEARIZED", penmanstr2linearized(penman_str))
+                        print("ENCODED", encoded)
+                        print("DECODED", decoded)
+                        print(original_tree)
+                        print(delinearized_tree)
+                        print(str(pfin))
+                        print(remove_wiki)
+                        raise ValueError("Tree mismatch between original tree and delinearized tree")
 
 
 if __name__ == "__main__":
