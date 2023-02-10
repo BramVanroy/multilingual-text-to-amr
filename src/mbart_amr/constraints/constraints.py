a2dc557984500d0de33341a642ed4346c38779cf
Bram Vanroy <Bram.Vanroy@UGent.be>
2023-02-10 16:50:25 +0100

    remove main

--- src/mbart_amr/constraints/constraints.py
+++ src/mbart_amr/constraints/constraints.py
@@ -20,34 +20,3 @@ class AMRLogitsProcessor(LogitsProcessor):
         scores = self.open_close_constraint(input_ids, scores)
         scores = self.allowed_constraint(input_ids, scores)
         return scores
-
-
-def main():
-    tokenizer = AMRMBartTokenizer.from_pretrained("BramVanroy/mbart-en-to-amr", src_lang="en_XX")
-    model = MBartForConditionalGeneration.from_pretrained("BramVanroy/mbart-en-to-amr")
-
-    model.resize_token_embeddings(len(tokenizer))
-
-    gen_kwargs = {"max_length": 36, "num_beams": 1}
-
-    if gen_kwargs.get("max_length") is None and gen_kwargs.get("max_new_tokens") is None:
-        gen_kwargs["max_length"] = model.config.max_length
-    gen_kwargs["num_beams"] = (
-        gen_kwargs["num_beams"] if gen_kwargs.get("num_beams") is not None else model.config.num_beams
-    )
-    gen_kwargs["logits_processor"] = LogitsProcessorList([AMRLogitsProcessor(tokenizer, gen_kwargs["max_length"])])
-
-    encoded = tokenizer("Should we go home now ?", return_tensors="pt")
-    generated = model.generate(
-        **encoded,
-        **gen_kwargs,
-    )
-
-    decoded = tokenizer.decode_and_fix(generated[0])[0]
-    print(generated)
-    print(decoded)
-    print(linearized2penmanstr(decoded))
-
-
-if __name__ == "__main__":
-    main()
