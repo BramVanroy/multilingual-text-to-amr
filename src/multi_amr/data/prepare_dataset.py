2340f7ed7276d8794afd9a7cca97fd39d3897fbc
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2023-08-02 09:31:24 +0200

    FEAT: add sacremoses normalization

--- src/multi_amr/data/prepare_dataset.py
+++ src/multi_amr/data/prepare_dataset.py
@@ -9,6 +9,7 @@ import penman
 from datasets import Dataset, DatasetDict
 from ftfy import fix_text
 from multi_amr.data.linearization import do_remove_wiki
+from sacremoses import MosesDetokenizer, MosesPunctNormalizer
 from tqdm import tqdm
 
 
@@ -37,19 +38,30 @@ def prepare_dataset(
     output_dir: Union[str, PathLike],
     dedupe: bool = False,
     remove_wiki: bool = False,
+    lang: str = "en",
 ):
-    """Given a directory of AMR files, deduplicate all files so that every file contains unique files
+    """Given a directory of AMR files, deduplicate all files so that every file contains unique files. We also process
+     the text for the sake of normalization. This is needed because the AMR3.0 corpus sometimes has unexpected
+     characters or encodings, sometimes the sentences are tokenized and sometimes they are not, and the punctuation
+     marks are inconsistent. So for the text, the data is ftfy.fix_text, punctuation normalized, detokenized. The AMR
+     is only ftfy and punctuation normalized.
 
-    :param amr_dirs: dirs with sub directories train, training, dev, valid, and or test. For multilingual datasets, you can enter multiple paths to their respective paths. Make sure to specify their respecitve language with 'src_langs'
+    :param amr_dirs: dirs with sub directories train, training, dev, valid, and or test. For multilingual datasets, you
+     can enter multiple paths to their respective paths. Make sure to specify their respecitve language with 'src_langs'
     :param src_langs: languages associated, in order, with 'amr_dirs'
     :param output_dir: dir to write the new structure and files to with fixed sentences
-    :param dedupe: whether to deduplicate the data. This will ensure that there will be no duplicates within and between splits
+    :param dedupe: whether to deduplicate the data. This will ensure that there will be no duplicates within and
+     between splits
     :param remove_wiki: whether to remove wiki from the AMR entries
+    :param lang: which language code is the text in (not the AMR)? This will be used for normalization
     """
 
     pdout = Path(output_dir).resolve()
     pdout.mkdir(exist_ok=True, parents=True)
-
+    punct_norm_amr = MosesPunctNormalizer(lang="en")
+    punct_norm_text = MosesPunctNormalizer(lang=lang)
+    detokenizer = MosesDetokenizer(lang=lang)
+    detokenize = detokenizer.detokenize
     data = {"metadata": [], "sentence": [], "penmanstr": [], "split_type": [], "src_lang": []}
 
     for amr_dir, src_lang in zip(amr_dirs, src_langs):
@@ -66,14 +78,23 @@ def prepare_dataset(
                         tree.reset_variables()
                         metadata = {**tree.metadata, "src_lang": src_lang}
                         data["metadata"].append(metadata)
-                        data["sentence"].append(tree.metadata["snt"])
+                        sentence = tree.metadata["snt"]
+                        # 1. Fix text
+                        # 2. Normalize punctuation (e.g. NLLB does not support en-dashes)
+                        # 3. Detokenize
+                        sentence = punct_norm_text.normalize(fix_text(sentence))
+                        sentence = detokenize(sentence.split())
+                        data["sentence"].append(sentence)
                         # It seems that some AMR is unparseable in some cases due to metadata (?; e.g. in test set)
                         # so we empty the metadata before continuing
                         tree.metadata = {}
 
                         # NOTE: the fix_text is important to make sure the reference tree also is correctly formed,
                         # e.g. (':op2', '"d’Intervention"'), -> (':op2', '"d\'Intervention"'),
-                        penman_str = fix_text(penman.format(tree))
+                        # 1. Fix
+                        # 2. Normalize punctuations -- note that this will remove indentation from the penman str,
+                        # but it should still work correctly when parsed with penman
+                        penman_str = punct_norm_amr.normalize((fix_text(penman.format(tree))))
                         if remove_wiki:
                             penman_str = do_remove_wiki(penman_str)
                         data["penmanstr"].append(penman_str)
@@ -121,12 +142,18 @@ def main():
     cparser.add_argument(
         "--dedupe",
         action="store_true",
-        help="whether to deduplicate the data. This will ensure that there will be no duplicates within and between splits",
+        help="whether to deduplicate the data. This will ensure that there will be no duplicates within and"
+        " between splits",
     )
     cparser.add_argument(
         "--remove_wiki",
         action="store_true",
         help="whether to remove wiki from the AMR entries",
+    )
+    cparser.add_argument(
+        "--lang",
+        default="en",
+        help="which language code is the text in (not the AMR)? This will be used for normalization.",
     )
 
     cargs = cparser.parse_args()
